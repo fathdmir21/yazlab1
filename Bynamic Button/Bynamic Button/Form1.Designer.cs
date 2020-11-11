d44badb757bbbbc1ad8c57c6d57d5886d557fb66
160202036 <61919387+160202036@users.noreply.github.com>
2020-11-11 18:55:08 +0300

    hadi

---
+++ Bynamic Button/Bynamic Button/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Bynamic_Button
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.yText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.xText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1780, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // yText
+            // 
+            this.yText.Location = new System.Drawing.Point(1780, 148);
+            this.yText.Name = "yText";
+            this.yText.Size = new System.Drawing.Size(100, 20);
+            this.yText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1777, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kaça Kaç Olsun?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // xText
+            // 
+            this.xText.Location = new System.Drawing.Point(1780, 89);
+            this.xText.Name = "xText";
+            this.xText.Size = new System.Drawing.Size(100, 20);
+            this.xText.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1780, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1800, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1041);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.xText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yText);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox yText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox xText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Bynamic Button/Bynamic Button/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bynamic_Button
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        int count=1;
+        int X = 0;
+        int Y = 0;
+        List<Button> btn_list = new List<Button>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            createBoard();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void createBoard()
+        {
+            //List<Button> btn_list;
+            int X = 0;
+            int Y = 0;
+            int x, y;
+            x = Convert.ToInt32(xText.Text);
+            y = Convert.ToInt32(yText.Text);
+            X = x;
+            Y = y;
+            int top = 50;
+            int left = 50;
+            int right = 50;
+            int count = 0;
+            for (int k = 0; k < X; k++)
+            {
+                left = left + 50;
+                for (int i = 0; i < Y; i++)
+                {
+
+                    Button button = new Button();
+                    button.Left = left;
+                    button.Size = new Size(35, 35);
+                    button.Top = top;
+                    this.Controls.Add(button);
+                    top += button.Height + 2;
+                    button.Name = count.ToString();
+                    count++;
+                    btn_list.Add(button);
+                    label2.Text = count.ToString();
+                }
+                top = 50;
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 NewForm = new Form1();
+            NewForm.Show();
+            this.Dispose(false);
+        }
+    }
+}
